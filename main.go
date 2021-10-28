6f26e4488a3c203968fcaf4f98e0b42e1fb4794e
aweehaw <92870706+aweehaw@users.noreply.github.com>
2021-10-28 23:13:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,14 +5,10 @@
 package main
 
 import (
-    "fmt" 		// this allows us to use fmt.Fprintf, fmt.Println, etc.
-    "net/http"	// this allows us to use the http handlers like http.HandleFunc and http.Handle
+    "fmt"
+    "net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {		// use http.HandleFunc() if you want a function to respond to the HTTP request.
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)			// let's say this page would open localhost:81 and if you go to localhost:81/test, it would print "Hello, you've requested: /test"
-    })
-
-    http.ListenAndServe(":81", nil) // this has 2 syntax, the first one is which port to use and the second one is the DefaultServeMux (which is Golang's default server)
+    fmt.Println("Hello World!");
 }
