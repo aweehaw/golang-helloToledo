05986c3046ae4f4c59c496578e8ca361fa5c3e9d
Miguel Batiquin <92870706+aweehaw@users.noreply.github.com>
2021-11-02 21:42:22 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
-// Test copy from https://github.com/gin-gonic/gin
+/*
+The sole purpose of this code is to create a cover photo for the group Toledo City Programmer's Club
+Let's put some additional text here to make it look cooler.
+Actually, we're also going to put the version.
+Let's say this program is version 1.01.01, created on November 2, 2021 GMT +08:00
+*/
 
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+)
+
+// We're creating a hello world message here, instead of world, we'll say Toledo.
+func helloToledo() string {
+	return "Hello, Toledo!"
+}
+
+func enterLocation() {
+	var location string
+	var locationResponse string
+	fmt.Print("\nPlease enter the City of your location: ")
+	fmt.Scan(&location)
+	if location == "Toledo" {
+		locationResponse = "Gwapo/gwapa pud diay ka. Hehe!"
+	} else if location == "Cebu" {
+		locationResponse = "Nice, duol duol ra. Hehe!"
+	} else {
+		locationResponse = "Apir! Welcome to the club."
+	}
+	fmt.Println(locationResponse)
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "ping",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	fmt.Println("\n")
+	fmt.Println(helloToledo())
+	fmt.Println("\n")
+	fmt.Println("Thanks for joining the Toledo City Programmer's Club.")
+	fmt.Println("Mga naa diri kay mga gwapo og gwapa.\n")
+	enterLocation()
 }
